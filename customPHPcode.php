669f0bafbd0faf37c6abea1d05dcf8ea61eae006
aftab70 <43289912+aftab70@users.noreply.github.com>
2022-06-03 02:47:57 +0530

    Update customPHPcode.php

--- customPHPcode.php
+++ customPHPcode.php
@@ -49,7 +49,7 @@ echo "You are connected with " .$localIP;
   <img src="https://media-exp1.licdn.com/dms/image/C4E03AQG9fcKbkqESuQ/profile-displayphoto-shrink_200_200/0/1641650801312?e=1658361600&v=beta&t=AOrlPXU43gtL0Aqr0Wa6KaRpGSJTLVwxAyLfmFfifCY" alt="" style="width:100%">
   <h1>Aftab Ali</h1>
   <p> Cloud and DevOps consultant </p>
-  <p> Deployed uisng Azure v1 </p>
+  <p> Deployed uisng Azure v2 </p>
   <div style="margin: 24px 0;"> 
     <a href="https://www.facebook.com/aftab.shaikh.589"><i class="fa fa-facebook"></i></a>  
     <a href="https://www.linkedin.com/in/aftab-shaikh-035440118/"><i class="fa fa-linkedin"></i></a>  
